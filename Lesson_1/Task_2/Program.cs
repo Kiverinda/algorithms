4c8532988690f4d9a41664dedbfbbc368a59268f
Dmitriy <kiverinda@yandex.ru>
2021-01-12 23:59:44 +0500

    Added Homework 1

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Сложность равна O(N^3)
+
+            // Решение:
+            // O(2 + N * N * 2N + N * N * (N - 1))
+            // O(3N^3 - N^2 + 2)
+            // После использования правил 3 и 5 сложность равна O(N^3)
+        }
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0; // O(1)
+            for (int i = 0; i < inputArray.Length; i++) 
+            {
+                for (int j = 0; j < inputArray.Length; j++) 
+                {
+                    for (int k = 0; k < inputArray.Length; k++) 
+                    {
+                        int y = 0; // O(N * N * N)
+                        if (j != 0) 
+                        {
+                            y = k / j; // O(N * N * (N - 1))
+                        }
+                        sum += inputArray[i] + i + k + j + y; // O(N * N * N)
+                    }
+                }
+            }
+            return sum; // O(1)
+        }
+    }
+}
---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int numberInSeries = GetNumberUser();
+            int[] arrayNymberFibonacci = new int[numberInSeries + 1];
+
+            //Вывод только заданного элемента последовательности Фибонначи 
+            Console.WriteLine(FibonacciRecursive(numberInSeries, ref arrayNymberFibonacci));
+            Console.WriteLine(Fibonacci(numberInSeries, ref arrayNymberFibonacci));
+            // Вывод всей последовательности Фибонначи от 0 до заданного элемента
+            foreach (int num in arrayNymberFibonacci)
+            {
+                Console.Write($"{num}  ");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int GetNumberUser()
+        {
+            int data;
+            do
+            {
+                Console.WriteLine("Введите целое число");
+            }
+            while (!int.TryParse(Console.ReadLine(), out data));
+            return data;
+        }
+
+        static int FibonacciRecursive(int numberInSeries, ref int[] arrayNymberFibonacci)
+        {
+            if (numberInSeries < 2)
+            {
+                arrayNymberFibonacci[1] = 1;
+                arrayNymberFibonacci[0] = 0;
+                return arrayNymberFibonacci[numberInSeries];
+            }
+            else arrayNymberFibonacci[numberInSeries] = FibonacciRecursive(numberInSeries - 1, ref arrayNymberFibonacci) + FibonacciRecursive(numberInSeries - 2, ref arrayNymberFibonacci);
+
+            return arrayNymberFibonacci[numberInSeries];
+        }
+
+        static int Fibonacci(int numberInSeries, ref int[] arrayNymberFibonacci)
+        {
+            arrayNymberFibonacci[1] = 1;
+            arrayNymberFibonacci[0] = 0;
+            for(int count = 2; count <= numberInSeries; count++)
+            {
+                arrayNymberFibonacci[count] = arrayNymberFibonacci[count -1] + arrayNymberFibonacci[count - 2];
+            }
+            return arrayNymberFibonacci[numberInSeries];
+        }
+
+    }
+}
