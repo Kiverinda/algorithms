778c9fb0d8ccf3f159ca86d704eb6444f4595dd6
Dmitriy <kiverinda@yandex.ru>
2021-01-25 22:58:25 +0500

    Added Homework 4

---
+++ Lesson_4/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Lesson_4/Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Tree treeTest = new Tree();
+            treeTest.Add(100);
+            treeTest.Add(99);
+            treeTest.Add(98);
+            treeTest.Add(97);
+            treeTest.Add(96);
+            treeTest.Add(95);
+            treeTest.Add(94);
+            treeTest.Add(93);
+            treeTest.Add(200);
+            treeTest.Add(50);
+            treeTest.Add(30);
+            treeTest.Add(24);
+            treeTest.Add(123);
+            treeTest.Add(150);
+            treeTest.Add(3);
+            TreePrinter.Print(treeTest.Head);
+            treeTest.Delete(98);
+            TreePrinter.Print(treeTest.Head);
+            treeTest.Find(96);
+            treeTest.Find(90);
+            Console.ReadLine();
+        }
+    }
+}
