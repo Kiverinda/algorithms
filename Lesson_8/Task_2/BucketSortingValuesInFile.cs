cc144e15071fbca9f74565ae2301ca262ec06a0d
Dmitriy <kiverinda@yandex.ru>
2021-02-12 19:36:49 +0500

    Added Homework 8

---
+++ Lesson_8/Task_2/BucketSortingValuesInFile.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    public class BucketSortingValuesInFile
+    {
+        public int NumberValuesInBucket { get; set; }
+        public string PathToIncomingData { get; set; }
+        public string PathToSortedData { get; set; }
+
+        public BucketSortingValuesInFile(string path, int numberValuesInBucket)
+        {
+            PathToIncomingData = path;
+            NumberValuesInBucket = numberValuesInBucket;
+            PathToSortedData = CreatingSortedBuckets();
+        }
+
+        string CreatingSortedBuckets()
+        {
+            List<string> pathsToBacket = new List<string>();
+            string secondLine = File.ReadLines(this.PathToIncomingData).First();
+
+            int countArray = 0;
+
+            while (secondLine != null)
+            {
+                int count = 0;
+                List<string> listValues = new List<string>();
+                while (secondLine != null && count < this.NumberValuesInBucket)
+                {
+                    listValues.Add(secondLine);
+                    count++;
+                    try
+                    {
+                        secondLine = File.ReadLines(this.PathToIncomingData).Skip(countArray * 10 + count).First();
+                    }
+                    catch
+                    {
+                        secondLine = null;
+                    }
+                }
+
+                List<int> temp = QuickSortBucket(ConvertStringListToIntList(listValues), 0, listValues.Count - 1);
+                File.WriteAllLines($"C:\\SourceValue\\backet_{countArray}.txt", ConvertIntListToStringList(temp));
+                pathsToBacket.Add($"C:\\SourceValue\\backet_{countArray}.txt");
+                countArray++;
+
+            }
+
+            //return pathsToBacket;
+            return MergingBuckets(pathsToBacket);
+        }
+
+        List<int> QuickSortBucket(List<int> list, int first, int last)
+        {
+            int i = first, j = last, x = list[(first + last) / 2];
+            do
+            {
+                while (list[i] < x)
+                    i++;
+                while (list[j] > x)
+                    j--;
+                if (i <= j)
+                {
+                    if (list[i] > list[j])
+                    {
+                        int tmp = list[i];
+                        list[i] = list[j];
+                        list[j] = tmp;
+                    }
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+            if (i < last)
+                QuickSortBucket(list, i, last);
+            if (first < j)
+                QuickSortBucket(list, first, j);
+            return list;
+        }
+
+        List<int> ConvertStringListToIntList(List<string> stringList)
+        {
+            List<int> intList = new List<int>(stringList.Count);
+            for (int i = 0; i < stringList.Count; i++)
+            {
+                intList.Add(Convert.ToInt32(stringList[i]));
+            }
+
+            return intList;
+        }
+
+        List<string> ConvertIntListToStringList(List<int> intList)
+        {
+            List<string> stringList = new List<string>(intList.Count);
+            for (int i = 0; i < intList.Count; i++)
+            {
+                stringList.Add(Convert.ToString(intList[i]));
+            }
+
+            return stringList;
+        }
+
+        string MergingBuckets(List<string> list)
+        {
+            string targetPath = $"C:\\SourceValue\\result.txt";
+            List<FirstValueInFile> listValue = new List<FirstValueInFile>();
+            for(int i = 0; i < list.Count; i++)
+            {
+                string pathToFile = $"C:\\SourceValue\\backet_{i}.txt";
+                listValue.Add(new FirstValueInFile(pathToFile));
+            }
+                
+            while(list.Count != 0)
+            {
+                for (int i = 0; i < listValue.Count; i++)
+                {
+                    try
+                    {
+                        string str = File.ReadLines(listValue[i].Path).Skip(listValue[i].NumberString).First();
+                        listValue[i].Value = int.Parse(str);
+                    }
+                    catch
+                    {
+                        if(listValue.Count > 1)
+                        {
+                            listValue.Remove(listValue[i]);
+                        }
+                        else
+                        {
+                            return targetPath;
+                        }
+                    }
+                }
+
+                if (listValue.Count > 1)
+                {
+                    FirstValueInFile min = listValue[0];
+                    for (int i = 1; i < listValue.Count; i++)
+                    {
+                        if (min.Value > listValue[i].Value)
+                        {
+                            min = listValue[i];
+                        }
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(targetPath, true, Encoding.UTF8))
+                    {
+                        writer.WriteLine(min.Value);
+                    }
+
+                    min.NumberString++;
+                }
+                else
+                {
+                    using (StreamWriter writer = new StreamWriter(targetPath, true, Encoding.UTF8))
+                    {
+                        writer.WriteLine(listValue[0].Value);
+                    }
+
+                    listValue[0].NumberString++;
+                }
+            }
+                
+            return targetPath;
+        }
+
+    }
+}
---
+++ Lesson_8/Task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text;
+using System.Linq;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\SourceValue\value.txt";
+            int numberElements = 35;
+            int numberElementsInBucket = 10;
+            CreatingFileWithSourceData(path, numberElements);
+            BucketSortingValuesInFile files = new BucketSortingValuesInFile(path, numberElementsInBucket);
+            Console.WriteLine(files.PathToSortedData);
+            Console.Read();
+        }
+
+        static void CreatingFileWithSourceData(string path, int number)
+        {
+            Random randomize = new Random();
+            string[] value = new string[number];
+            for (int i = 0; i < value.Length; i++)
+            {
+                value[i] = Convert.ToString(randomize.Next(500));
+                Console.WriteLine(value[i]);
+            }
+            File.WriteAllLines(path, value);
+        }
+        
+    }
+}
